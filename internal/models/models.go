8ad5dbd2098bd6540bd73e9ed98ea2feb72319ef
JoePeach762 <123075586+JoePeach762@users.noreply.github.com>
2026-01-13 00:54:41 +0500

    little changes

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,8 +1,37 @@
 package models
 
+import (
+	"time"
+)
+
 type StudentInfo struct {
 	ID    uint64
 	Name  string
 	Email string
 	Age   uint64
+}
+
+type UserInfo struct {
+	ID              uint64
+	Name            string //John
+	Email           string //John@gmail.com
+	WeightKg        uint8  //80
+	TargetWeightKg  uint8  //95
+	Age             uint8  //24
+	Sex             string //Male
+	CurrentCalories uint16
+	CurrentProteins uint16
+	CurrentFats     uint16
+	CurrentCarbs    uint16
+}
+
+type MealInfo struct {
+	ID           uint64
+	Name         string  //Grilled chicken
+	WeightGramms float32 //200
+	Calories100g float32
+	Proteins100g float32
+	Fats100g     float32
+	Carbs100g    float32
+	Date         time.Time
 }
